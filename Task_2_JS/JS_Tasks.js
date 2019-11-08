d94c8381bbc7c9ae1e91cabb3ed0b76bf258109e
Ilyaleshka <oleshko.ivl@gmail.com>
2019-11-08 12:55:39 +0300

    added new tasks

--- Task_2_JS/JS_Tasks.js
+++ Task_2_JS/JS_Tasks.js
@@ -343,29 +343,24 @@ function splice(sourceString, start, delCount, newSubStr) {
 }
 
 function getFormatText(sourceString, maxLineSize = 0, maxLineCount = 0, formatType = "NONE") {
-    let currLineSize = 0;
-    let currLineCount;
     symbolFormat(sourceString, maxLineSize, maxLineCount)
 }
 
-function wordFormat(sourceString, maxLineSize, maxLineCount) {
-
-
-
-    let currWord = "";
+function wordFormat(sourceString, maxLineSize = 0, maxLineCount = 0) {
     let lineWordsCount = 0;
-
-    let currLineSize = 0;
+    let currLineSize = 1;
     let currLineCount = 1;
     let i = 0;
+
     while (i < sourceString.length) {
 
         if (sourceString[i] == " ") {
-            if ((maxLineSize == 0) || ((currLineSize + 1) <= maxLineSize)) {
+            if ((maxLineSize == 0) || (((currLineSize + 1) <= maxLineSize) && (currLineSize <= maxLineSize))) {
                 currLineSize++;
             } else {
+                lineWordsCount = 0;
                 sourceString = splice(sourceString, i, 0, '\n');
-                currLineSize = 0;
+                currLineSize = 1;
                 currLineCount++;
                 i++;
                 if ((maxLineCount != 0) && (currLineCount > maxLineCount)) {
@@ -378,8 +373,9 @@ function wordFormat(sourceString, maxLineSize, maxLineCount) {
         }
 
         if (sourceString[i] == "\n") {
-            currLineSize = 0;
+            currLineSize = 1;
             currLineCount++;
+            lineWordsCount = 0;
             i++
             if ((maxLineCount != 0) && (currLineCount > maxLineCount)) {
                 sourceString = sourceString.slice(0, i - 1);
@@ -389,35 +385,37 @@ function wordFormat(sourceString, maxLineSize, maxLineCount) {
         }
 
         let j = i;
-        while((j < sourceString.length) && (sourceString[i] != "\n") && (sourceString[i] != " "))
-        {
+        while ((j + 1 < sourceString.length) && (sourceString[j + 1] != "\n") && (sourceString[j + 1] != " ")) {
+            let buf = sourceString[j];
             j++;
         }
 
-        if((maxLineSize == 0) || ((currLineSize + (j + 1 - i) <= maxLineSize) || (lineWordsCount == 0)) )
-        {
-            if(maxLineSize == 0)
-            {
-                currLineSize += i - j + 1;
-                i = j;
-                i++
-                continue;
-            }
-
-            if((lineWordsCount == 0) && (currLineSize + (j + 1 - i) > maxLineSize) && (currLineSize != 0))
-            {
-                sourceString = splice(sourceString, i, 0, '\n');
-                currLineSize = 0;
-                currLineCount++;
-                i++;
-                j++;
-                if ((maxLineCount != 0) && (currLineCount > maxLineCount)) {
-                    sourceString = sourceString.slice(0, i - 1);
-                    break;
-                }
-            }
-            currLineSize += i - j + 1;
+        if (maxLineSize == 0) {
+            currLineSize += j - i + 1;
             i = j;
+            i++
+            lineWordsCount++;
+            continue;
+        }
+
+        if (((currLineSize + (j - i) <= maxLineSize) || (lineWordsCount == 0))) {
+            lineWordsCount++;
+            currLineSize += j - i + 1;
+            i = j;
+            i++;
+            continue;
+        }
+
+        if ((currLineSize + (j - i) > maxLineSize) && (currLineSize != 0)) {
+            sourceString = splice(sourceString, i, 0, '\n');
+            currLineSize = 1;
+            currLineCount++;
+            lineWordsCount = 0;
+            i++;
+            if ((maxLineCount != 0) && (currLineCount > maxLineCount)) {
+                sourceString = sourceString.slice(0, i - 1);
+                break;
+            }
         }
 
         i++;
@@ -425,14 +423,14 @@ function wordFormat(sourceString, maxLineSize, maxLineCount) {
     return sourceString;
 }
 
-function symbolFormat(sourceString, maxLineSize, maxLineCount) {
-    let currLineSize = 0;
+function symbolFormat(sourceString, maxLineSize = 0, maxLineCount = 0) {
+    let currLineSize = 1;
     let currLineCount = 1;
     let i = 0;
     while (i < sourceString.length) {
 
         if (sourceString[i] == "\n") {
-            currLineSize = 0;
+            currLineSize = 1;
             currLineCount++;
             i++
             if ((maxLineCount != 0) && (currLineCount > maxLineCount)) {
@@ -446,7 +444,7 @@ function symbolFormat(sourceString, maxLineSize, maxLineCount) {
             currLineSize++;
         } else {
             sourceString = splice(sourceString, i, 0, '\n');
-            currLineSize = 0;
+            currLineSize = 1;
             currLineCount++;
             if ((maxLineCount != 0) && (currLineCount > maxLineCount)) {
                 sourceString = sourceString.slice(0, i - 1);
@@ -459,10 +457,381 @@ function symbolFormat(sourceString, maxLineSize, maxLineCount) {
     return sourceString;
 }
 
-function sentenceFormat(sourceString, maxLineSize, maxLineCount) {
+function sentenceFormat(sourceString, maxLineSize = 0, maxLineCount = 0) {
+    let lineWordsCount = 0;
+    let currLineSize = 1;
+    let currLineCount = 1;
+    let i = 0;
 
+    while (i < sourceString.length) {
+
+        if (sourceString[i] == "\n") {
+            currLineSize = 1;
+            currLineCount++;
+            lineWordsCount = 0;
+            i++
+            if ((maxLineCount != 0) && (currLineCount > maxLineCount)) {
+                sourceString = sourceString.slice(0, i - 1);
+                break;
+            }
+            continue;
+        }
+
+        let j = i;
+        while ((j < sourceString.length) && (sourceString[j] != "\n") && (sourceString[j] != ".")) {
+            let buf = sourceString[j];
+            j++;
+        }
+
+        if (maxLineSize == 0) {
+            currLineSize += j - i + 1;
+            i = j;
+            i++
+            lineWordsCount++;
+            continue;
+        }
+
+        if (((currLineSize + (j - i) <= maxLineSize) || (lineWordsCount == 0))) {
+            lineWordsCount++;
+            currLineSize += j - i + 1;
+            i = j;
+            i++;
+            continue;
+        }
+
+        if ((currLineSize + (j - i) > maxLineSize) && (currLineSize != 0)) {
+            sourceString = splice(sourceString, i, 0, '\n');
+            currLineSize = 1;
+            currLineCount++;
+            lineWordsCount = 0;
+            i++;
+            if ((maxLineCount != 0) && (currLineCount > maxLineCount)) {
+                sourceString = sourceString.slice(0, i - 1);
+                break;
+            }
+        }
+
+        i++;
+    }
+    return sourceString;
 }
-console.log("Symbols:");
-console.log(symbolFormat("tessd gjfsdft\nsdfsghdfsdjgh f\nsdfs ddfsd  fsdf\nsdfdfsdfsdfs dfsdfsddgsdg sdg", 7, 6));
+//console.log("Symbols:");
+//console.log(symbolFormat("tessd gjfsdft\nsdfsghdfsdjgh f\nsdfs ddfsd  fsdf\nsdfdfsdfsdfs dfsdfsddgsdg sdg", 7, 6));
+let str = "Halloween, the time of pumpkins, candies, ghosts, witches and much more, is annually celebrated on 31 October. That's the night before All Saints Day. Its origins date back thousands of years to the Celtic festival of Samhaim or The Feast of the Sun, a most significant holiday of the Celtic year. This day marked the end of summer but also the season of darkness as well as the beginning of the New Year on 1 November."
+/*
+console.log(str);
+console.log();
 console.log("word:");
-console.log(wordFormat("tessd gjfsdft\nsdfsghdfsdjgh f\nsdfs ddfsd  fsdf\nsdfdfsdfsdfs dfsdfsddgsdg sdg", 7, 6));
+console.log(wordFormat(str));
+console.log();
+console.log("symbol:");
+console.log(symbolFormat(str));
+console.log();
+console.log("symbol:");
+console.log(sentenceFormat(str));
+*/
+
+/*
+4.	String calculator
+Разработать объект содержащий набор методов для выполнения функций 
+калькулятора над числами передаваемыми в строковом представлении.
+ Методы должны позволять работать как в целочисленном, так и вещественном формате.
+ */
+
+function getAdditionResult(num1, num2, floatFormat = false) {
+    let var1, var2;
+
+    if (!floatFormat) {
+        var1 = parseInt(num1, 10);
+        var2 = parseInt(num2, 10);
+    } else {
+        var1 = parseFloat(num1);
+        var2 = parseFloat(num2);
+    }
+
+    if (isNaN(num1) || isNaN(num2))
+        throw "Invalid arguments";
+
+    return var1 + var2;
+}
+
+function getMultiplicationResult(num1, num2, floatFormat = false) {
+    let var1, var2;
+
+    if (!floatFormat) {
+        var1 = parseInt(num1, 10);
+        var2 = parseInt(num2, 10);
+    } else {
+        var1 = parseFloat(num1);
+        var2 = parseFloat(num2);
+    }
+
+    if (isNaN(num1) || isNaN(num2))
+        throw "Invalid arguments";
+
+    return var1 * var2;
+}
+
+function getSubtractionResult(num1, num2, floatFormat = false) {
+    let var1, var2;
+
+    if (!floatFormat) {
+        var1 = parseInt(num1, 10);
+        var2 = parseInt(num2, 10);
+    } else {
+        var1 = parseFloat(num1);
+        var2 = parseFloat(num2);
+    }
+
+    if (isNaN(num1) || isNaN(num2))
+        throw "Invalid arguments";
+
+    return var1 + var2;
+}
+
+function getDivisionResul(num1, num2, floatFormat = false) {
+    let var1, var2;
+
+    if (!floatFormat) {
+        var1 = parseInt(num1, 10);
+        var2 = parseInt(num2, 10);
+    } else {
+        var1 = parseFloat(num1);
+        var2 = parseFloat(num2);
+    }
+
+    if (isNaN(num1) || isNaN(num2))
+        throw "Invalid arguments";
+
+    return var1 / var2;
+}
+
+function getDifference(num1, num2, floatFormat = false) {
+    let var1, var2;
+
+    if (!floatFormat) {
+        var1 = parseInt(num1, 10);
+        var2 = parseInt(num2, 10);
+    } else {
+        var1 = parseFloat(num1);
+        var2 = parseFloat(num2);
+    }
+
+    if (isNaN(num1) || isNaN(num2))
+        throw "Invalid arguments";
+
+    return var1 - var2;
+}
+
+//console.log(getSum("100000.5", "0.5", true));
+
+function BubbleSort(sourceArray) {
+    if ((!sourceArray.constructor === Array) && (sourceArray.length == 0))
+        return "argument isn't array";
+
+    let end = false;
+    for (let i = 0; i < sourceArray.length; i++) {
+        if (end)
+            break;
+
+        end = true;
+
+        for (let j = 0; j < (sourceArray.length - 1) - i; j++) {
+            if (sourceArray[j] < sourceArray[j + 1]) {
+                let buf = sourceArray[j];
+                sourceArray[j] = sourceArray[j + 1];
+                sourceArray[j + 1] = buf;
+                end = false;
+            }
+        }
+    }
+
+    return sourceArray;
+}
+
+function BubbleSort(sourceArray) {
+    if ((!sourceArray.constructor === Array) && (sourceArray.length == 0))
+        return "argument isn't array";
+
+    let end = false;
+    for (let i = 0; i < sourceArray.length; i++) {
+        if (end)
+            break;
+
+        end = true;
+
+        for (let j = 0; j < (sourceArray.length - 1) - i; j++) {
+            if (sourceArray[j] < sourceArray[j + 1]) {
+                let buf = sourceArray[j];
+                sourceArray[j] = sourceArray[j + 1];
+                sourceArray[j + 1] = buf;
+                end = false;
+            }
+        }
+    }
+
+    return sourceArray;
+}
+
+function SelectionSort(sourceArray) {
+    if ((!sourceArray.constructor === Array) && (sourceArray.length == 0))
+        throw "argument isn't array";
+
+    for (let i = 0; i < sourceArray.length; i++) {
+        let max = sourceArray[i];
+        let index = i;
+        for (let j = i + 1; j < sourceArray.length; j++) {
+            if (sourceArray[j] > max) {
+                max = sourceArray[j];
+                index = j;
+            }
+        }
+        let buf = sourceArray[i];
+        sourceArray[i] = sourceArray[index];
+        sourceArray[index] = buf;
+    }
+
+    return sourceArray;
+}
+
+function SelectionSort(sourceArray) {
+    if ((!sourceArray.constructor === Array) && (sourceArray.length == 0))
+        throw "argument isn't array";
+
+    for (let i = 0; i < sourceArray.length; i++) {
+        let max = sourceArray[i];
+        let index = i;
+        for (let j = i + 1; j < sourceArray.length; j++) {
+            if (sourceArray[j] > max) {
+                max = sourceArray[j];
+                index = j;
+            }
+        }
+        let buf = sourceArray[i];
+        sourceArray[i] = sourceArray[index];
+        sourceArray[index] = buf;
+    }
+
+    return sourceArray;
+}
+
+
+function QuickSort(sourceArray, left, right) {
+    let l = left;
+    let r = right;
+
+    if (r - l <= 1) return;
+
+    let t = (l + (r - l) / 2)
+    let mid = sourceArray[Math.ceil(l + (r - l) / 2)];
+    let ll = l,
+        rr = r - 1;
+
+    while (ll <= rr) {
+        while (sourceArray[ll] > mid) ll++;
+        while (sourceArray[rr] < mid) rr--;
+        if (ll <= rr) {
+            let buf = sourceArray[ll];
+            sourceArray[ll] = sourceArray[rr];
+            sourceArray[rr] = buf;
+            ll++;
+            rr--;
+        }
+    }
+    if (l < rr) {
+        QuickSort(sourceArray, l, rr + 1);
+    }
+    if (ll < r) {
+        QuickSort(sourceArray, ll, r);
+    }
+
+    return sourceArray;
+}
+
+function QuickSort(sourceArray, left, right) {
+    let l = left;
+    let r = right;
+
+    if (r - l <= 1) return;
+
+    let mid = sourceArray[Math.ceil(l + (r - l) / 2)];
+    let ll = l,
+        rr = r - 1;
+
+    while (ll <= rr) {
+        while (sourceArray[ll] > mid) ll++;
+        while (sourceArray[rr] < mid) rr--;
+        if (ll <= rr) {
+            let buf = sourceArray[ll];
+            sourceArray[ll] = sourceArray[rr];
+            sourceArray[rr] = buf;
+            ll++;
+            rr--;
+        }
+    }
+    if (l < rr) {
+        QuickSort(sourceArray, l, rr + 1);
+    }
+    if (ll < r) {
+        QuickSort(sourceArray, ll, r);
+    }
+
+    return sourceArray;
+}
+
+function gShellSort(sourceArray) {
+    let l = 0;
+    let r = sourceArray.length - 1;
+    let sz = sourceArray.length;
+    let step = sz / 2;
+    while (step >= 1) {
+        for (let i = l + step; i < r; i++) {
+            let j = i;
+            let diff = Math.ceil(j - step);
+            while (diff >= l && sourceArray[diff] > sourceArray[j]) {
+                let buf = sourceArray[diff];
+                sourceArray[j] = sourceArray[diff];
+                sourceArray[diff] = buf;
+                j = diff;
+                diff = Math.ceil(j - step);
+            }
+        }
+        step /= 2;
+    }
+    return sourceArray;
+}
+
+function ShellSort(sourceArray) // * ∆k = (b∆k−1)/2  ∆0 = N
+{
+    let step, i, j, tmp;
+    let size = sourceArray.length;
+    // Выбор шага
+    for (step = Math.round(size / 1.7); step > 1; step = Math.round(step / 1.7)) {
+        // Перечисление элементов, которые сортируются на определённом шаге
+        for (i = step; i < size; i++) {
+            let temp = sourceArray[i];
+            // Перестановка элементов внутри подсписка, пока i-тый не будет отсортирован
+            for (j = i; (j >= step ) && (temp < sourceArray[j - step]); j -= step) {
+                sourceArray[j] = sourceArray[j - step];
+            }
+            sourceArray[j] = temp;
+        }
+    }
+
+    return sourceArray;
+}
+
+
+let arr = [0, 5, 7, -7, 5, -3, 8, 4, -10, 5, 3, 65, -4, 6, 0, -7, 6, 3, -8, -1, 5, 11, 3];
+console.log("bubble");
+console.log(BubbleSort(arr.slice()));
+console.log("selection");
+console.log(SelectionSort(arr.slice()));
+console.log("quick");
+console.log(QuickSort(arr.slice(), 0, arr.length));
+console.log("shell");
+console.log(ShellSort(arr.slice()));
+/*let t = BubbleSort(arr)
+for (i = 0; i < t.length; i++) {
+    console.log(t[i]);
+}*/
