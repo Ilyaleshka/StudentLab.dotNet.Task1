fea273abce015efa30de4a16d67e859aa2a8c063
Ilyaleshka <oleshko.ivl@gmail.com>
2019-11-25 10:54:14 +0300

    completed task 4

--- Task_4_SQL/FillTablesSQLQuery.sql
+++ Task_4_SQL/FillTablesSQLQuery.sql
@@ -1,9 +1,11 @@
+
+create database trading_network
+
 use trading_network
 
-
---create table Shops (shop_code int primary key, shop_name varchar(30),shop_address varchar(50))
 --drop table Shops
-/*
+create table Shops (shop_code int primary key, shop_name varchar(30),shop_address varchar(50))
+
 TRUNCATE TABLE  Shops;
 INSERT INTO Shops
            (shop_code
@@ -13,11 +15,11 @@ INSERT INTO Shops
            (1,'evroopt','minsk, belarus'),
            (2,'5element','grodno'),
            (3,'ikea','new york')
-*/
 
---create table Products (product_code int primary key, product_name varchar(30),product_cost int)
+
+create table Products (product_code int primary key, product_name varchar(30),product_cost int)
 --drop table Products
-/*
+
 TRUNCATE TABLE  Products;
 INSERT INTO Products
            (product_code
@@ -31,28 +33,33 @@ INSERT INTO Products
            (6,'beer',30),
            (7,'mobile phone',1000),
            (8,'bicycle',1500)
-*/
 
---create table Orders (order_code int primary key, products_count int,order_date date, order_cost int)
+
+create table Orders (order_code int primary key, products_count int,order_date date, order_cost int)
 --drop table Orders
 
-/*
+
 TRUNCATE TABLE  Orders ;
+
 INSERT INTO Orders 
            (order_code
            ,products_count
            ,order_date
            ,order_cost)
      VALUES
-           (1,5,'2015-12-13',150),
-           (2,1,'2015-01-5',30),
-           (3,10,'2016-03-25',240),
-           (4,7,'2015-01-17',180);
-*/
+        --  (1,5,'2015-12-13',150),
+        --  (2,1,'2015-01-5',30),
+        --  (3,10,'2016-03-25',240),
+        --  (4,7,'2015-01-17',180),
+        --(143,434,'2015-01-17',18000000),
+        (654,64,'2014-01-17',500000),
+        (655,345,'2014-06-01',500001)
 
---create table Customers (customer_code int primary key, customer_name varchar(30))
+
+
+create table Customers (customer_code int primary key, customer_name varchar(30))
 --drop table Customers
-/*
+
 TRUNCATE TABLE  Customers ;
 INSERT INTO Customers 
            (customer_code
@@ -62,11 +69,11 @@ INSERT INTO Customers
            (2,'Greg'),
            (3,'Roma'),
            (4,'Alex');
-*/
 
 
 
-/*
+
+
          
 create table ShopsProducts 
 	(shop_code int not null,
@@ -74,9 +81,9 @@ create table ShopsProducts
 	 UNIQUE (shop_code , product_code),
 	 FOREIGN KEY (shop_code) REFERENCES Shops (shop_code),
 	 FOREIGN KEY (product_code ) REFERENCES Products (product_code))
-*/
+
 --drop table ShopsProducts
- /*
+
  INSERT INTO ShopsProducts 
            (shop_code
            ,product_code)
@@ -85,21 +92,21 @@ create table ShopsProducts
            (2,1),
            (3,1),
            (3,2)
-*/
 
 
 
 
-/*create table OrdersProducts 
+
+create table OrdersProducts 
 	(order_code int not null,
 	 product_code int not null,
 	 UNIQUE (order_code , product_code),
 	 FOREIGN KEY (order_code) REFERENCES Orders (order_code),
 	 FOREIGN KEY (product_code ) REFERENCES Products (product_code))
-*/
+
 
 --drop table OrdersProducts
-/*
+
  INSERT INTO OrdersProducts  
            (order_code
            ,product_code)
@@ -112,26 +119,76 @@ create table ShopsProducts
            (3,6),
            (4,7),
            (4,4)
-*/
 
 
-/*create table UserOrders 
+
+create table UserOrders 
 	(order_code int not null,
 	 customer_code int not null,
 	 UNIQUE (order_code),
 	 FOREIGN KEY (order_code) REFERENCES Orders (order_code),
 	 FOREIGN KEY (customer_code ) REFERENCES Customers (customer_code))
-*/
 
 --drop table UserOrders
 
- /*
+ 
  INSERT INTO UserOrders 
            (order_code
            ,customer_code)
      VALUES
-           (1,1),
-           (2,2),
-           (3,3),
-           (4,4)
-*/
+         --  (1,1),
+         --  (2,2),
+         --  (3,3),
+         --  (4,4),
+         --(143,2),
+         --(654,3),
+         --(655,3)
+
+
+create table ShopsOrders 
+	(shop_code int not null,
+	 order_code int not null,
+	 UNIQUE (order_code),
+	 FOREIGN KEY (order_code) REFERENCES Orders (order_code),
+	 FOREIGN KEY (shop_code) REFERENCES Shops (shop_code))
+
+
+--drop table ShopsOrders 
+
+ 
+ INSERT INTO ShopsOrders  
+           (shop_code
+           ,order_code)
+     VALUES
+		(1,1),
+		(2,2),
+		(2,3),
+		(3,4),
+		(3,143),
+		(2,654),
+		(1,655)
+
+
+
+create table ProductsSale 
+	(product_code int not null,
+	 product_count int not null,
+	 sale_date date)
+
+
+--drop table ProductsSale  
+
+
+ INSERT INTO ProductsSale  
+           (product_code
+            ,product_count
+            ,sale_date)
+     VALUES
+		(1,8,'2015-01-17'),
+		(2,5,'2015-01-03'),
+		(1,4,'2014-12-17'),
+		(3,5,'2014-12-28'),
+		(2,6,'2014-12-02'),
+		(4,7,'2016-01-21'),
+		(5,15,'2014-03-17')
+		
--- Task_4_SQL/TradeNetworkSQLQuery.sql
+++ Task_4_SQL/TradeNetworkSQLQuery.sql
@@ -2,48 +2,63 @@
 
 use trading_network
 
---create table Shops (shop_code int primary key, shop_name varchar(30),shop_address varchar(50))
---create table Products (product_code int primary key, product_name varchar(30),product_cost int)
---create table Orders (order_code int primary key, products_count int,order_date date, order_cost int)
---create table Customers (customer_code int primary key, customer_name varchar(30))
+---------------------------------------1)+	Получить все содержимое таблицы Product ---------------------------------------------------------------------
 
-
-
---1)	Получить все содержимое таблицы Product
 Select * from Products
 
---2)	Получить коды и названия товаров, минимальная стоимость которых не превышают 500
+---------------------------------------2)+	Получить коды и названия товаров, минимальная стоимость которых не превышают 500-----------------------------
+
 Select product_code,product_name from Products where (product_cost < 500);
 
---3)	Получить среднюю стоимость заказов, сделанных в январе 2015 года
-Select avg(order_cost) as average_cost from Orders where order_date between '20150101' and '20150201';
+---------------------------------------3)+	Получить среднюю стоимость заказов, сделанных в январе 2015 года---------------------------------------------
 
---4)	Получить название самого дорого товара
+Select avg(order_cost) as 'average cost' from Orders where order_date between '20150101' and '20150201';
+
+---------------------------------------4)+	Получить название самого дорого товара-----------------------------------------------------------------------
+
 Select TOP 1 product_name from Products order by product_cost desc;
 
---5)	Найти названия товаров, которые были куплены в январе 2015
+---------------------------------------5)+	Найти названия товаров, которые были куплены в январе 2015---------------------------------------------------
+
 Select DISTINCT p.product_name
 From Products p
 Join OrdersProducts op ON op.product_code = p.product_code 
 Join Orders o ON o.order_code = op.order_code
 where o.order_date between '20150101' and '20150201';
 
---6)	Найти названия товаров, продажи которых в январе 2015 упали по сравнению с предыдущим месяцем
+---------------------------------------6)+	Найти названия товаров, продажи которых в январе 2015 упали по сравнению с предыдущим месяцем----------------
 
+Select  p.product_name
+From Products p
+Join ProductsSale jsales ON ( jsales.product_code = p.product_code) and ( jsales.sale_date between '20150101' and '20150201')
+Join ProductsSale psales ON (psales.product_code = p.product_code) and (psales.sale_date between '20141201' and '20150101')
+GROUP BY
+    p.product_name
+having
+	sum(jsales.product_count)< sum(psales.product_count)
 
---7)	Найти среднюю стоимость товаров в каждом магазине
-Select DISTINCT  s.shop_name, AVG(p.product_cost)
+---------------------------------------7)+	Найти среднюю стоимость товаров в каждом магазине-------------------------------------------------------------
+
+Select s.shop_name, AVG(p.product_cost)
 From Shops s
 Join ShopsProducts sp ON sp.shop_code = s.shop_code 
 Join Products p ON p.product_code = sp.product_code
 GROUP BY
     s.shop_name;
 
---8)	Найти значение продаж в каждом магазине за 2015-й год
+---------------------------------------8)+	Найти значение продаж в каждом магазине за 2015-й год---------------------------------------------------------
 
+Select  s.shop_name, SUM(o.order_cost)
+From Shops s
+Join ShopsOrders so ON so.shop_code = s.shop_code
+Join Orders o ON so.order_code = o.order_code
+where o.order_date between '20150101' and '20160101'
+group by 
+	shop_name
 
---9)	Показать всех покупателей, покупки которых в 2014-м превысили 1.000.000
-Select DISTINCT  c.customer_name
+---------------------------------------9)+	Показать всех покупателей, покупки которых в 2014-м превысили 1.000.000---------------------------------------
+
+Select c.customer_name
 From Customers c
 Join UserOrders co ON co.customer_code = c.customer_code 
 Join Orders o ON o.order_code = co.order_code
@@ -53,7 +68,11 @@ group by
 having
 	sum(o.order_cost) > 1000000;
 
---10)	Увеличить стоимость все продуктов в полтора раза, если она не превышает 100
---UPDATE Products SET product_cost = product_cost * 1.5 where product_cost <= 100
-
---11)	Удалить из таблицы order все записи за 2014 и более ранние годы
+---------------------------------------10)+	Увеличить стоимость все продуктов в полтора раза, если она не превышает 100------------------------------------
+/*
+UPDATE Products SET product_cost = product_cost * 1.5 where product_cost <= 100
+*/
+---------------------------------------11)+	Удалить из таблицы order все записи за 2014 и более ранние годы------------------------------------------------
+/*
+Delete from Orders where order_date < '20150101'
+*/
